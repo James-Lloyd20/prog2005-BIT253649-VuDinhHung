cfa2f84c9006269d7493f1055f1336a0aba30ce0
James-Lloyd20 <109863811+James-Lloyd20@users.noreply.github.com>
2026-01-30 16:04:25 +0700

    Delete Ex2.py

--- Ex2.py
+++
@@ -1,6 +0,0 @@
-# Bài 2: Yêu cầu: Tính chu vi hình tròn Định nghĩa hằng số PI = 3.14. Tính và in ra chu vi hình tròn với bán kính r = 5
-
-PI = 3.14
-r = 5
-chu_vi_h_tron = 2*PI*r
-print(f'Chu vi hinh tron voi ban kinh {r} la: ({chu_vi_h_tron})')
