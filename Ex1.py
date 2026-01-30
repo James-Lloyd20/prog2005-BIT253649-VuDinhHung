ee1fea2358b519d16b85b3309473e2d251e222c0
James-Lloyd20 <109863811+James-Lloyd20@users.noreply.github.com>
2026-01-30 16:01:43 +0700

    Delete Ex1.py Ex2.py Ex3.py

--- Ex1.py
+++
@@ -1,8 +0,0 @@
-# Bài 1: Yêu cầu: Tạo các biến với các kiểu dữ liệu khác nhau. Tạo và in ra ba biến: một số nguyên (integer), một số thực (float), và một chuỗi (string)
-
-so_nguyen = 12
-print(so_nguyen)
-so_thuc = 9.23
-print(so_thuc)
-chuoi = (f'Tren day la so nguyen va so thuc. So nguyen la: {so_nguyen}; so thuc la: {so_thuc}')
-print(chuoi)
