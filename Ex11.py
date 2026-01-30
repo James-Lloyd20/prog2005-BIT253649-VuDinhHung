4887846298c4cc55110ee40a6ceb876900d56f42
James-Lloyd20 <109863811+James-Lloyd20@users.noreply.github.com>
2026-01-30 16:02:30 +0700

    Delete Ex11.py

--- Ex11.py
+++
@@ -1,6 +0,0 @@
-# Bài 11: Yêu cầu: Chuyển độ C sang độ F Viết một chương trình chuyển nhiệt độ từ độ C sang độ F. Yêu cầu người dùng nhập nhiệt độ theo độ C (kiểu float). Sử dụng công thức: F = C × 9/5 + 32. In kết quả ra dạng chuỗi định dạng.
-
-do_c = float(input("Vui long nhap nhiet do (do C): "))
-do_f = do_c * 9/5 + 32
-
-print(f"{do_c} do C = {do_f} do F")
