474f55516d7881bdf391e5c8a8bdca5c5b789574
James-Lloyd20 <109863811+James-Lloyd20@users.noreply.github.com>
2026-01-30 16:05:43 +0700

    Delete Ex4.py

--- Ex4.py
+++
@@ -1,6 +0,0 @@
-# Bài 4: Yêu cầu: Hàm tính tổng hai số. Viết một hàm sum_two_numbers(a, b) trả về tổng của hai số nguyên. Gọi hàm và in ra kết quả
-
-def sum_two_numbers(a, b):
-    return a + b
-ket_qua = sum_two_numbers(12, 34)
-print(f'Tong cua hai so nguyen la: {ket_qua}')
